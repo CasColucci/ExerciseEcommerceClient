94a6111da986f9041c7eb30e736786b6ac3a1334
Cas <51390039+CasColucci@users.noreply.github.com>
2024-09-18 17:18:18 -0400

    Adding empty pages and working out controller

--- EcommerceClient/Controllers/ProductController.cs
+++ EcommerceClient/Controllers/ProductController.cs
@@ -1,11 +1,64 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EcommerceClient.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace EcommerceClient.Controllers
 {
     public class ProductController : Controller
     {
-        private string BaseUrl = "https://localhost:7213/";
-        public IActionResult Index()
+        private string baseUrl = "https://localhost:7213/";
+        public async Task<IActionResult> Index()
+        {
+            List<Product> products = new List<Product>();
+            using (var _httpClient = new HttpClient()) 
+            { 
+                _httpClient.BaseAddress = new Uri(baseUrl + "api/");
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage getData = await _httpClient.GetAsync("GetAllProducts");
+
+                if (getData.IsSuccessStatusCode)
+                {
+                    string result = getData.Content.ReadAsStringAsync().Result;
+                    products = JsonConvert.DeserializeObject<List<Product>>(result);
+                }
+                else
+                {
+                    return View("ErrorPage");
+                }
+            }
+            return View(products);
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> CreateProduct(Product product)
+        {
+            using (var _httpClient = new HttpClient())
+            {
+                _httpClient.BaseAddress = new Uri(baseUrl + "api/");
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage getData = await _httpClient.PutAsJsonAsync("", product);
+
+                if (getData.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View("ErrorPage");
+                }
+            }
+        }
+
+        public IActionResult ErrorPage()
         {
             return View();
         }
